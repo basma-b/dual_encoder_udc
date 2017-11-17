39c36e3adf5321f3ad4d629db1ea6cb468d44069
Basma BOUSSAHA <ab_boussaha@esi.dz>
2017-11-17 13:36:40 +0100

    Update dual_encoder.py

--- dual_encoder.py
+++ dual_encoder.py
@@ -1,14 +1,11 @@
 # -*- encoding:utf-8 -*-
 from __future__ import print_function
 
-import os
-import sys
 import numpy as np
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 import pickle
 from keras.models import Sequential
-from keras.models import load_model as K_load_model
 from keras.utils import np_utils
 from keras.layers import Dense, Input, Flatten, Dropout, LSTM, Merge, Activation
 from keras.layers import Conv1D, MaxPooling1D, Embedding, merge
@@ -16,7 +13,6 @@ from keras.models import Model
 from utilities import my_callbacks
 import argparse
 from utilities.data_helper import compute_recall_ks, str2bool
-#import cPickle
 
 def main():
     
@@ -60,23 +56,11 @@ def main():
             continue
         embeddings_index[word] = coefs
     f.close()
-
-    print("Now loading UDC data...")
-    
-    train_c, train_r, train_l = pickle.load(open(args.input_dir + 'train.pkl', 'rb'))
-    test_c, test_r, test_l = pickle.load(open(args.input_dir + 'test.pkl', 'rb'))
-    dev_c, dev_r, dev_l = pickle.load(open(args.input_dir + 'dev.pkl', 'rb'))
-    
-    print('Found %s training samples.' % len(train_c))
-    print('Found %s dev samples.' % len(dev_c))
-    print('Found %s test samples.' % len(test_c))
     
     MAX_SEQUENCE_LENGTH, MAX_NB_WORDS, word_index = pickle.load(open(args.input_dir + 'params.pkl', 'rb'))
     MAX_SEQUENCE_LENGTH = 160
     print("MAX_SEQUENCE_LENGTH: {}".format(MAX_SEQUENCE_LENGTH))
     print("MAX_NB_WORDS: {}".format(MAX_NB_WORDS))
-    
-    
     
     print("Now loading embedding matrix...")
     num_words = min(MAX_NB_WORDS, len(word_index)) + 1
@@ -90,33 +74,43 @@ def main():
             embedding_matrix[i] = embedding_vector
 
     print("Now building dual encoder lstm model...")
-    # define lstm for sentence1
-    m_model = Sequential()
-    m_model.add(Embedding(output_dim=args.emb_dim,
+    # define lstm encoder
+    encoder = Sequential()
+    encoder.add(Embedding(output_dim=args.emb_dim,
                             input_dim=MAX_NB_WORDS,
                             input_length=MAX_SEQUENCE_LENGTH,
                             weights=[embedding_matrix],
                             mask_zero=True,
                             trainable=True))
     
-    m_model.add(LSTM(units=args.hidden_size))
+    encoder.add(LSTM(units=args.hidden_size))
     
     context_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
     response_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
 
-    # these two models will share eveything from shared_cnn
-    context_branch = m_model(context_input)
-    response_branch = m_model(response_input)
+    # encode the context and the response
+    context_branch = encoder(context_input)
+    response_branch = encoder(response_input)
     
     concatenated = merge([context_branch, response_branch], mode='mul')
     out = Dense((1), activation = "sigmoid") (concatenated)
 
-    model = Model([context_input, response_input], out)
-    model.compile(loss='binary_crossentropy',
+    dual_encoder = Model([context_input, response_input], out)
+    dual_encoder.compile(loss='binary_crossentropy',
                     optimizer=args.optimizer)
     
-    print(m_model.summary())
-    print(model.summary())
+    print(encoder.summary())
+    print(dual_encoder.summary())
+    
+    print("Now loading UDC data...")
+    
+    train_c, train_r, train_l = pickle.load(open(args.input_dir + 'train.pkl', 'rb'))
+    test_c, test_r, test_l = pickle.load(open(args.input_dir + 'test.pkl', 'rb'))
+    dev_c, dev_r, dev_l = pickle.load(open(args.input_dir + 'dev.pkl', 'rb'))
+    
+    print('Found %s training samples.' % len(train_c))
+    print('Found %s dev samples.' % len(dev_c))
+    print('Found %s test samples.' % len(test_c))
     
     print("Now training the model...")
     
@@ -128,20 +122,10 @@ def main():
     print("\tbatch_size={}, nb_epoch={}".format(args.batch_size, args.n_epochs))
     
     for ep in range(1, args.n_epochs):
-        
-        #model.fit([train_c, train_r], train_l,
-                #batch_size=args.batch_size, nb_epoch=1, callbacks=[histories],
-                #validation_data=([dev_c, dev_r], dev_l), verbose=1)
                 
-        model.fit([train_c, train_r], train_l,
+        dual_encoder.fit([train_c, train_r], train_l,
                 batch_size=args.batch_size, epochs=1, callbacks=[histories],
                 validation_data=([dev_c, dev_r], dev_l), verbose=1)
-        
-        #model.fit([train_c[:100], train_r[:100]], train_l[:100],
-                #batch_size=args.batch_size, epochs=1, callbacks=[histories],
-                #validation_data=([dev_c[:100], dev_r[:100]], dev_l[:100]), verbose=1)
-
-        #model.save(model_name + "_ep." + str(ep) + ".h5")
 
         curAcc =  histories.accs[0]
         if curAcc >= bestAcc:
@@ -150,21 +134,20 @@ def main():
         else:
             patience = patience + 1
 
-        #doing classify the test set
-        y_pred = model.predict([test_c, test_r])        
-        #y_pred = model.predict([test_c[:100], test_r[:100]])     
+        # classify the test set
+        y_pred = dual_encoder.predict([test_c, test_r])          
         
         print("Perform on test set after Epoch: " + str(ep) + "...!")    
         recall_k = compute_recall_ks(y_pred[:,0])
         
-        #stop the model whch patience = 10
+        # stop training the model when patience = 10
         if patience > 10:
             print("Early stopping at epoch: "+ str(ep))
             break
         
     if args.save_model:
         print("Now saving the model... at {}".format(args.model_fname))
-        model.save(args.model_fname)
+        dual_encoder.save(args.model_fname)
 
 if __name__ == "__main__":
     main()
